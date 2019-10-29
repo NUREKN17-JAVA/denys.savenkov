eb8d200702cadb9fc6e987a08096379b08188fd3
denys.savenkov <denys.savenkov@gmail.com>
2019-10-29 09:24:59 +0200

    Creating a test for the HsqldbUserDao.create(User user) method

---
+++ usermanagement/src/test/java/ua/nure/cs/savenkov/usermanagement/db/HsqldbUserDaoTest.java
@@ -0,0 +1,50 @@
+package ua.nure.cs.savenkov.usermanagement.db;
+
+import java.util.Date;
+
+import org.dbunit.DatabaseTestCase;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.IDataSet;
+
+import ua.nure.cs.savenkov.usermanagement.User;
+
+public class HsqldbUserDaoTest extends DatabaseTestCase {
+
+  private static final String LASTNAME = "Doe";
+  private static final String FIRSTNAME = "John";
+  private HsqldbUserDao dao;
+  
+  public void testCreate() throws DataBaseException {
+    try {
+		User user = new User();
+		user.setFirstName(FIRSTNAME);
+		user.setLastName(LASTNAME);
+		user.setDateOfBirth(new Date());
+		assertNull(user.getId());
+		user = dao.create(user);
+		assertNotNull(user);
+		assertNotNull(user.getId());
+	} catch (Exception e) {
+		e.printStackTrace();
+		fail(e.toString());
+	}
+  }
+
+  protected void setUp() throws Exception {
+    super.setUp();
+    dao = new HsqldbUserDao();
+  }
+
+  @Override
+  protected IDatabaseConnection getConnection() throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  protected IDataSet getDataSet() throws Exception {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
