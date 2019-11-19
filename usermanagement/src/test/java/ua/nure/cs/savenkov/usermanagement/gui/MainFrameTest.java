03e2d8cc472db651de6533342b05512748981851
denys.savenkov <denys.savenkov@gmail.com>
2019-11-19 10:45:41 +0200

    Add User Panel Test

--- usermanagement/src/test/java/ua/nure/cs/savenkov/usermanagement/gui/MainFrameTest.java
+++ usermanagement/src/test/java/ua/nure/cs/savenkov/usermanagement/gui/MainFrameTest.java
@@ -1,23 +1,38 @@
 package ua.nure.cs.savenkov.usermanagement.gui;
 
 import java.awt.Component;
+import java.text.DateFormat;
+import java.util.Date;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 
 import junit.extensions.jfcunit.JFCTestCase;
 import junit.extensions.jfcunit.JFCTestHelper;
 import junit.extensions.jfcunit.TestHelper;
+import junit.extensions.jfcunit.eventdata.MouseEventData;
+import junit.extensions.jfcunit.eventdata.StringEventData;
 import junit.extensions.jfcunit.finder.NamedComponentFinder;
+import junit.framework.TestCase;
 
 public class MainFrameTest extends JFCTestCase {
 
+	private static final String DATE_OF_BIRTH_FIELD_COMPONENT_NAME = "dateOfBirthField";
+	private static final String LAST_NAME_FIELD_COMPONENT_NAME = "lastNameField";
+	private static final String FIRST_NAME_FIELD_COMPONENT_NAME = "firstNameField";
+	private static final String ADD_PANEL_COMPONENT_NAME = "addPanel";
 	private static final String DETAILS_BUTTON_COMPONENT_NAME = "detailsButton";
 	private static final String EDIT_BUTTON_COMPONENT_NAME = "editButton";
 	private static final String ADD_BUTTON_COMPONENT_NAME = "addButton";
 	private static final String DELETE_BUTTON_COMPONENT_NAME = "deleteButton";
 	private static final String USER_TABLE_COMPONENT_NAME = "userTable";
 	private static final String BROWSE_PANEL_COMPONENT_NAME = "browsePanel";
+	private static final String OK_BUTTON_COMPONENT_NAME = "okButton";
+	private static final String FIRST_NAME = "John";
+	private static final String LAST_NAME = "Doe";
+	private static final Date DATE_OF_BIRTH = new Date();
 	private MainFrame mainFrame;
 
 	protected void setUp() throws Exception {
@@ -45,9 +60,36 @@ public class MainFrameTest extends JFCTestCase {
 	public void testBrowseControls() {
 		find(JPanel.class, BROWSE_PANEL_COMPONENT_NAME);
 		find(JTable.class, USER_TABLE_COMPONENT_NAME);
-		find(JPanel.class, ADD_BUTTON_COMPONENT_NAME);
-		find(JPanel.class, EDIT_BUTTON_COMPONENT_NAME);
-		find(JPanel.class, DETAILS_BUTTON_COMPONENT_NAME);
+		find(JButton.class, ADD_BUTTON_COMPONENT_NAME);
+		find(JButton.class, EDIT_BUTTON_COMPONENT_NAME);
+		find(JButton.class, DETAILS_BUTTON_COMPONENT_NAME);
+		find(JButton.class, DELETE_BUTTON_COMPONENT_NAME);
 	}
+	
+	public void testAddUserOk() {
+		find(JPanel.class, BROWSE_PANEL_COMPONENT_NAME);
+		JButton addButton = (JButton) find(JButton.class, ADD_BUTTON_COMPONENT_NAME);
+		getHelper().enterClickAndLeave(new MouseEventData(this, addButton));
+		find(JPanel.class, ADD_PANEL_COMPONENT_NAME);
+		fillFields(FIRST_NAME, LAST_NAME, DATE_OF_BIRTH);
+		
+		JButton okButton = (JButton) find(JButton.class, OK_BUTTON_COMPONENT_NAME);
+		getHelper().enterClickAndLeave(new MouseEventData(this, okButton));
+		
+		find(JPanel.class, BROWSE_PANEL_COMPONENT_NAME);
+		// check dao results
+	}
+
+	private void fillFields(String firstName, String lastName, Date dateOfBirth) {
+		JTextField firstNameField = (JTextField) find(JTextField.class, FIRST_NAME_FIELD_COMPONENT_NAME);
+		JTextField lastNameField = (JTextField) find(JTextField.class, LAST_NAME_FIELD_COMPONENT_NAME);
+		JTextField dateOfBirthField = (JTextField) find(JTextField.class, DATE_OF_BIRTH_FIELD_COMPONENT_NAME);
+		
+		getHelper().sendString(new StringEventData(this, firstNameField, firstName));
+		getHelper().sendString(new StringEventData(this, lastNameField, lastName));
+		String dateString = DateFormat.getInstance().format(dateOfBirth);
+		getHelper().sendString(new StringEventData(this, dateOfBirthField, dateString));
+	}
+	
 
 }
