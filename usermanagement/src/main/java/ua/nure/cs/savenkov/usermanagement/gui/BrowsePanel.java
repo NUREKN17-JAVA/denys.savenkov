c0246b707a338fe4c0050706c11e2295c086bccb
denys.savenkov <denys.savenkov@gmail.com>
2019-11-19 10:00:03 +0200

    Created a constructor for main frame

---
+++ usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/gui/BrowsePanel.java
@@ -0,0 +1,10 @@
+package ua.nure.cs.savenkov.usermanagement.gui;
+
+import javax.swing.JPanel;
+
+public class BrowsePanel extends JPanel {
+	private MainFrame parent;
+	public BrowsePanel(MainFrame mainFrame) {
+		parent = mainFrame;
+	}
+}
--- usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/gui/MainFrame.java
+++ usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/gui/MainFrame.java
@@ -1,7 +1,45 @@
 package ua.nure.cs.savenkov.usermanagement.gui;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MainFrame extends JFrame {
+	private static final int HEIGHT = 600;
+	private static final int WIDTH = 800;
+	private JPanel contentPanel;
+	private BrowsePanel browsePanel;
+	
+	public MainFrame() {
+		super();
+		// dao
+		initialize();
+	}
 
+	private void initialize() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(WIDTH, HEIGHT);
+		this.setTitle("Управление пользователями"); // localize
+		this.setContentPane(getContentPanel());
+		
+	}
+
+	private JPanel getContentPanel() {
+		if (contentPanel == null) {
+			contentPanel = new JPanel();
+			contentPanel.setLayout(new BorderLayout());
+			contentPanel.add(getBrowsePanel(), BorderLayout.CENTER);
+		}
+		return contentPanel;
+	}
+
+	private BrowsePanel getBrowsePanel() {
+		if (browsePanel == null) {
+			browsePanel = new BrowsePanel(this);
+		}
+		return browsePanel;
+	}
 }
