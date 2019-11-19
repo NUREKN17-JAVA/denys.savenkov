108d7838f947e2b091c212bc68ab1c0518834735
denys.savenkov <denys.savenkov@gmail.com>
2019-11-19 08:50:08 +0200

    Refactoring DaoFactory for further GUI implementation

--- usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/db/DaoFactory.java
+++ usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/db/DaoFactory.java
@@ -7,48 +7,55 @@ import sun.security.jca.GetInstance.Instance;
 import ua.nure.cs.savenkov.usermanagement.User;
 
 
-public class DaoFactory {
+public abstract class DaoFactory {
 	private static final String SETTING_PROPERTIES = "settings.properties";
 	private static final String CONNECTION_DRIVER = "connection.driver";
 	private static final String CONNECTION_URL = "connection.url";
 	private static final String CONNECTION_PASSWORD = "connection.password";
 	private static final String CONNECTION_USER = "connection.user";
-	private static final String USER_DAO = "dao.usermanagement.db.UserDao";
-	private final Properties properties;
+	protected static final String USER_DAO = "dao.UserDao";
+	private static final String DAO_FACTORY = "dao.factory";
 	
-	private static final DaoFactory INSTANCE = new DaoFactory();
+	protected static Properties properties;
+	protected static DaoFactory instance;
 	
-	public DaoFactory() {
+	protected DaoFactory() {}
+	
+	static {
 		properties = new Properties();
 		try {
-			properties.load(getClass().getClassLoader().getResourceAsStream(SETTING_PROPERTIES));
+			properties.load(DaoFactory.class.getClass().getClassLoader().getResourceAsStream(SETTING_PROPERTIES));
 		}
 		catch (IOException e){
 			throw new RuntimeException(e);
 		}
 	}
 	
-	public static DaoFactory getInstance() {
-        return INSTANCE;
-    }
-	
-	private ConnectionFactory getConnectionFactory() {
-		String user = properties.getProperty(CONNECTION_USER);
-		String password = properties.getProperty(CONNECTION_PASSWORD);
-		String url  = properties.getProperty(CONNECTION_URL);
-		String driver = properties.getProperty(CONNECTION_DRIVER);
-		return new ConnectionFactoryImpl(driver, url, user, password);
+	public static synchronized DaoFactory getInstance() {
+		if (instance == null){
+	    		try {
+				Class factoryClass = Class.forName(properties
+						.getProperty(DAO_FACTORY));
+				instance = (DaoFactory) factoryClass.newInstance();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+	   	}
+	       return instance;
 	}
 	
-	public Dao<User> getUserDao() {
-		Dao<User> result = null;
-        try {
-            Class clazz = Class.forName(properties.getProperty(USER_DAO));
-            result = (Dao<User>) clazz.newInstance();
-            result.setConnectionFactory(getConnectionFactory());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return result;
+	protected abstract ConnectionFactory getConnectionFactory();
+//	String user = properties.getProperty(CONNECTION_USER);
+//	String password = properties.getProperty(CONNECTION_PASSWORD);
+//	String url  = properties.getProperty(CONNECTION_URL);
+//	String driver = properties.getProperty(CONNECTION_DRIVER);
+//	return new ConnectionFactoryImpl(driver, url, user, password);
+	
+	public abstract Dao<User> getUserDao();
+	
+	
+	public void init(Properties properties) {
+		this.properties = properties;
+		instance = null;
 	}
 }
