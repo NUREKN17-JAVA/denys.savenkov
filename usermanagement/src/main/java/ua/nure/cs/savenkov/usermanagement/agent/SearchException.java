0053dfc118ceaaedf69f5a153e88833675287b42
denys.savenkov <denys.savenkov@gmail.com>
2019-12-25 17:21:08 +0200

    Implemented SearchException

---
+++ usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/agent/SearchException.java
@@ -0,0 +1,17 @@
+package ua.nure.cs.savenkov.usermanagement.agent;
+
+
+public class SearchException extends Exception {
+    private String name;
+
+    public SearchException(Exception e) {
+        this.name = e.toString();
+    }
+
+    public String getName() { return name; }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
