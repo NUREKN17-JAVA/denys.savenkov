c5f4e1017369d3a353425c68d850b0ba6f999f6e
denys.savenkov <denys.savenkov@gmail.com>
2019-12-25 16:41:32 +0200

    Initialized SearchAgent

---
+++ usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/agent/SearchAgent.java
@@ -0,0 +1,15 @@
+package ua.nure.cs.savenkov.usermanagement.agent;
+
+import jade.core.Agent;
+
+public class SearchAgent extends Agent {
+	protected void setup() {
+		super.setup();
+		System.out.println(getAID().getName() + " started");
+	}
+	
+	protected void takeDown() {
+		System.out.println(getAID().getName() + " terminated");
+		super.takeDown();
+	}
+}
