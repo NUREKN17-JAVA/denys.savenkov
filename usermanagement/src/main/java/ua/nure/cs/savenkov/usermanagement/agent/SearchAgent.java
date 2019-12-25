55131bd5dc54ffc41e25634a0b5f227f6ebcda72
denys.savenkov <denys.savenkov@gmail.com>
2019-12-25 17:39:36 +0200

    Implement SearchRequestBehaviour

--- usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/agent/SearchAgent.java
+++ usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/agent/SearchAgent.java
@@ -1,6 +1,11 @@
 package ua.nure.cs.savenkov.usermanagement.agent;
 
+import java.util.Collection;
+
+import jade.core.AID;
 import jade.core.Agent;
+import ua.nure.cs.savenkov.usermanagement.db.DaoFactory;
+import ua.nure.cs.savenkov.usermanagement.db.DataBaseException;
 
 public class SearchAgent extends Agent {
 	protected void setup() {
@@ -11,5 +16,23 @@ public class SearchAgent extends Agent {
 	protected void takeDown() {
 		System.out.println(getAID().getName() + " terminated");
 		super.takeDown();
+	}
+	
+	public void search(String firstName, String lastName) throws SearchException{
+		try {
+			Collection users = DaoFactory.getInstance().getUserDao().find(firstName, lastName);
+			if (users.size() > 0) {
+				showUsers(users);
+			} else {
+				addBehaviour(new SearchRequestBehaviour(new AID[] {}, firstName, lastName));
+			}
+			
+		} catch (DataBaseException e ) {
+			throw new SearchException(e);
+		}
+	}
+	
+	private void showUsers(Collection user) {
+		// TODO Отобразить найденных пользователей
 	}
 }
