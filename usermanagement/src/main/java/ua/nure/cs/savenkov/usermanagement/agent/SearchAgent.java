7338c2849bc42a075f1275dc6367a8d430ef7687
denys.savenkov <denys.savenkov@gmail.com>
2019-12-25 17:45:16 +0200

    Implement RequestServer

--- usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/agent/SearchAgent.java
+++ usermanagement/src/main/java/ua/nure/cs/savenkov/usermanagement/agent/SearchAgent.java
@@ -32,7 +32,7 @@ public class SearchAgent extends Agent {
 		}
 	}
 	
-	private void showUsers(Collection user) {
+	void showUsers(Collection user) {
 		// TODO Отобразить найденных пользователей
 	}
 }
